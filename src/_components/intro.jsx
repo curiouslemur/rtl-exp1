00116652b8e1c48e66896a830483656f23fc3b7e
Noëlle R <noelle.tiana@gmail.com>
2024-09-25 14:14:46 -0700

    .

--- src/_components/intro.jsx
+++ src/_components/intro.jsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState, createContext } from "react";
 import { Button, Grid } from "@mui/material"
 // import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw';
 
 import * as ic from "../_controllers/introController"
 
 const IntroContext = createContext()
 
 export const Intro = (props) => {
+    const [intro_md, setIntro_md] = useState('');
+
     const [cannotStart, setCannotStart] = useState(true)
 
     useEffect(() => {
         const timeout = setTimeout(() => {
             setCannotStart(false)
         }, 3000) // TODO: make this like 30s instead 
+
+        fetch(props.expLang + "/intro-bar.md")
+            .then((response) => response.text()).then((text) => { console.log(text); setIntro_md(text) }).catch((err) => console.error(err));
+
         return () => clearTimeout(timeout);
     }, []);
 
@@ -24,7 +33,11 @@ export const Intro = (props) => {
                 <Grid item xl={6} xs={10}>
                     <hr style={{ color: "#9c27b0", backgroundColor: "#9c27b0", height: 2 }} />
 
-                    <props.expPages.Intro keywordColor="#ea3433" expLang={props.expLang} /> <br />
+                    <div className="markdown-content">
+                        <ReactMarkdown children={intro_md} rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]} />
+                    </div>
+
+                    {/* <props.expPages.Intro keywordColor="#ea3433" expLang={props.expLang} /> <br /> */}
 
                     <Button variant='contained' style={{ marginTop: '5ch' }}
                         disabled={cannotStart}
--- src/locals/english/introEn.jsx
+++ src/locals/english/introEn.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 // import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import rehypeRaw from 'rehype-raw';
+// import ReactMarkdown from 'react-markdown'
+// import remarkGfm from 'remark-gfm'
+// import rehypeRaw from 'rehype-raw';
 
 export const IntroEn = (props) => {
-    const [intro_md, setIntro_md] = useState('');
+    // const [intro_md, setIntro_md] = useState('');
 
     useEffect(() => {
-        fetch(props.expLang + "/intro-bar.md")
-            .then((response) => response.text()).then((text) => { console.log(text); setIntro_md(text) }).catch((err) => console.error(err));
+        // fetch(props.expLang + "/intro-bar.md")
+        //     .then((response) => response.text()).then((text) => { console.log(text); setIntro_md(text) }).catch((err) => console.error(err));
     },);
 
     return (
         <>
             <Grid container align='justify'> </Grid >
             <Grid>
-                <div className="markdown-content">
+                {/* <div className="markdown-content">
                     <ReactMarkdown children={intro_md} rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]} />
-                </div>
+                </div> */}
 
                 {/* <Typography>
                     <br /> You can access this instruction page at anytime by clicking on the help icon <HelpOutlineIcon style={{ marginLeft: 5, marginRight: 5 }} fontSize="medium" />
@@ -29,5 +29,5 @@ export const IntroEn = (props) => {
     )
 }
 
-export const IntroLabelsEn = { start: "Start", }
+export const IntroLabelsEn = { start: "Start" }
 export default IntroEn;
