da7e6dea88d1437139da408a17bed94aa80389fc
Noëlle R <noelle.tiana@gmail.com>
2024-09-25 13:45:59 -0700

    fixed consent md issue

--- src/locals/english/consentEn.jsx
+++ src/locals/english/consentEn.jsx
@@ -11,21 +11,27 @@ const styles = {
 }
 
 export const ConsentEn = (props) => {
+    const [intro_md, setIntro_md] = useState('');
+
     useEffect(() => {
-        fetch('/consent.md')
+        // fetch('rtl-exp1/en/consent.md')
+        fetch(props.expTitle + "/" + props.expLang + "/consent.md")
             .then((response) => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
                 return response.text();
             })
-            .then((text) => console.log(text))
+            .then((text) => setIntro_md(text))
             .catch((err) => console.error('Error fetching the Markdown file:', err));
     }, []);
 
     return (
         <Grid align='justify'>
-            <Typography paragraph style={styles}> Before starting the study, please read this page carefully.</Typography>
+            <div className="markdown-content">
+                <ReactMarkdown children={intro_md} rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]} />
+            </div>
+            {/* <Typography paragraph style={styles}> Before starting the study, please read this page carefully.</Typography>
             <Typography paragraph><b style={{ color: props.keywordColor }}>The goal of this research is </b> to understand how people read data visualization to form their most intuitive answer to a given question.</Typography>
             <Typography paragraph><b style={{ color: props.keywordColor }}>Procedure: </b> You will read about a fictitious context for which data was collected and visually presented.
                 The study asks questions about what you see on the visualizations. You will have to <i>identify a scpecific element</i> or <i>count elements in the chart.</i>
@@ -37,12 +43,11 @@ export const ConsentEn = (props) => {
             <Typography paragraph><b style={{ color: props.keywordColor }}>Record keeping: </b> Records of your participation will be held confidential so far as permitted by law.
                 However, the study investigators and, under certain circumstances, the Worcester Polytechnic Institute Institutional Review Board (WPI IRB) will be able to inspect and have access to this data.</Typography>
             <Typography paragraph><b style={{ color: props.keywordColor }}>Risks: </b> We do not expect there to be any risks beyond daily use of computer devices.</Typography>
-            {/* <Typography paragraph><b style={{ color: props.keywordColor }}>Contact info: </b> Prof. L. Harrison, ltharrison@wpi.edu</Typography> */}
             <Typography paragraph><b style={{ color: props.keywordColor }}>Your participation in this research is voluntary.</b> We do not expect there to be any risks. Your refusal to participate will not result in any penalty to you or any loss of benefits to which you may otherwise be entitled.
                 You may decide to stop participating at any time without penalty or loss of other benefits. </Typography>
             <Typography paragraph>For more information about your rights as a research participant, or any concerns related to this study, please contact Prof. Lane Harrison (ltharrison@wpi.edu)</Typography>
             <br />
-            <Typography paragraph>By filling in the following form, and clicking 'NEXT', you agree to participate in the study.</Typography>
+            <Typography paragraph>By filling in the following form, and clicking 'NEXT', you agree to participate in the study.</Typography> */}
         </Grid>
     )
 }
