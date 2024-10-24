85c23845100255dbcdc8287204f30e1c9c61af32
Noëlle R <noelle.tiana@gmail.com>
2024-10-24 08:39:35 -0700

    update mobile limit

--- src/locales/english_/consentEn.jsx
+++ src/locales/english_/consentEn.jsx
@@ -6,29 +6,6 @@ export const ConsentEn = (props) => {
 
 
 export const ConsentLabelsEn = {
-    mobileWarning: "Please set your browser to full screen, or use a device or a computer with a wider display.",
-    consentTitle: "Welcome",
-
-    countryResQ: "From which country are you taking this survey? *",
-    countryResLabel: "Country of residence",
-    countryResLenQ: "How many years have you lived there? *",
-    countryResLenLabel: "",
-    countryResLongestQ: "In which country have you lived the longest? *",
-    countryResLongestLabel: "Longest country of residence",
-
-    langNativeQ: "What is your native language? *",
-    langNativeLabel: "Mother tongue",
-    langOtherQ: "What other languages do you know fluently?",
-    langOtherLabel: "List all that apply",
-
-    ageQ: "How old are you? *", ageLabel: "Age",
-    genderQ: "What is your gender? *", genderLabel: "Gender",
-
-    professionQ: "What is your profession? *", professionLabel: "Profession",
-
-    visFamiliarityQ: "How often do you use charts or data visualizations?",
-    visFamiliarityLabel: "",
-    sign: "Sign"
 }
 
 export default ConsentEn;
