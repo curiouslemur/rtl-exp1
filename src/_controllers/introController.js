cdb819fe730fd1722bb119b74511a1e1ec9026ef
Noëlle R <noelle.tiana@gmail.com>
2024-10-31 18:19:10 -0700

    stringify language list

--- src/_controllers/introController.js
+++ src/_controllers/introController.js
@@ -1,12 +1,10 @@
 
-// import * as d3 from 'd3'
-// import { colorCoord } from '../stimuli/colors'
-
 import * as dao from '../_utils/firebase-config'
 
 export const onClickStart = (navigate, nextUrl) => {
     // add tuto answers info from sessionStrorage to gemography: tuto: "[array here]" 
     let dem = JSON.parse(sessionStorage.getItem('demography'))
+    dem.languageOther = JSON.stringify(dem.languageOther)
     sessionStorage.setItem('demography', JSON.stringify(dem))
     dao.logDem(dem)
     navigate(nextUrl)
